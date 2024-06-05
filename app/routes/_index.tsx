09c50ec884130db95bff03479004ebd9d430cc9c
takeuchi <takeuchi.hi@tsunagu-grp.jp>
2024-06-05 13:29:47 +0900

    first

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json, Form, useActionData, useLoaderData } from '@remix-run/react'
+import { Todo } from '../../domain/Todo'
+import { supabase } from '../../lib/supabase'
+import { useEffect } from 'react'
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +11,113 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const response = await supabase.from('todo').select('*')
+  const data = response.data
+
+  if (!data) {
+    return json({ todos: [] })
+  }
+
+  const todos = data.map((todo) => new Todo(todo.id, todo.title, todo.done))
+
+  return json({ todos })
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+
+  if (formData.has('action')) {
+    const action = formData.get('action') as string
+    if (action === 'delete') {
+      const id = formData.get('id') as string
+      await supabase.from('todo').delete().eq('id', id)
+    }
+
+    if (action === 'create') {
+      const title = formData.get('title') as string
+      if (!title) {
+        return json({ error: 'タイトルを入力してください' }, {status: 400})
+      }
+      await supabase.from('todo').insert([{ title, done: false }])
+
+    }
+  }
+
+  return null
+}
+
 export default function Index() {
+  const { todos } = useLoaderData() as { todos: Todo[] }
+  const actionData = useActionData<{error: string}>()
+
+  useEffect(() => {
+    if (!actionData?.error) {
+      closeModal();
+      (document.getElementById("title") as HTMLInputElement).value = "";
+    }
+  }, [actionData]);
+
+  const closeModal = () => {
+    (document.getElementById('my_modal_1') as HTMLDialogElement).close()
+  }
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="font-sans m-80">
+      <h1 className="text-2xl font-bold mb-8">やることリスト</h1>
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className="flex items-center border-b border-gray-300 py-2"
+        >
+          <div className="flex-grow">{todo.title}</div>
+          <Form method="post">
+            <input type="hidden" name="id" value={todo.id}/>
+            <button name="action" value="delete" className="text-red-500" type="submit">
+              ☓
+            </button>
+          </Form>
+        </div>
+      ))}
+      <div className="mt-8 flex justify-end">
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            (document.getElementById('my_modal_1') as HTMLDialogElement).showModal()
+          }}
+        >
+          新規登録
+        </button>
+      </div>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">新規登録</h3>
+          <div className="label-text mb-4">やること</div>
+          <Form method="post">
+            {actionData?.error && (
+              <div className="text-red-500">{actionData.error}</div>
+            )}
+            <input
+              type="text"
+              className="input input-bordered w-full mb-4"
+              name="title"
+              id="title"
+            />
+            <div className="flex">
+              <button
+                className="btn btn-primary mr-4"
+                type="submit"
+                name="action"
+                value="create"
+              >
+                登録
+              </button>
+              <button className="btn" type="button" onClick={closeModal}>
+                閉じる
+              </button>
+            </div>
+          </Form>
+        </div>
+      </dialog>
     </div>
   );
 }
